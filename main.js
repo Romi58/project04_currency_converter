9725bf9a75bdc74a045c9810ed423e054fc55925
Romi58 <romaisanaz67@gmail.com>
2024-05-14 00:22:04 +0500

    update readme.md

---
+++ main.js
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+const currencies = [
+    { name: 'US Dollar (USD)', rate: 1 },
+    { name: 'Euro (EUR)', rate: 0.85 },
+    { name: 'Pound Sterling (GBP)', rate: 0.72 },
+    { name: 'Japanese Yen (JPY)', rate: 0.0077 },
+    { name: 'Pakistani Rupee (PKR)', rate: 0.0054 },
+];
+const questions = [
+    {
+        type: 'input',
+        name: 'amount',
+        message: 'Enter the amount in US Dollars:',
+        validate: (value) => {
+            const valid = !isNaN(Number(value));
+            return valid || 'Please enter a valid number.';
+        },
+    },
+    {
+        type: 'list',
+        name: 'targetCurrency',
+        message: 'Choose the target currency:',
+        choices: currencies.map((currency) => currency.name),
+    },
+];
+let targetCurrency;
+inquirer.prompt(questions).then((answers) => {
+    targetCurrency = currencies.find((currency) => currency.name === answers.targetCurrency);
+    const usd = parseFloat(answers.amount);
+    const convertedAmount = usd * targetCurrency.rate;
+    console.log(`The amount in ${targetCurrency.name} is: ${convertedAmount.toFixed(2)}`);
+});
